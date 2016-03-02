c1d3a5a46e02635a37d86ca3a404b6cfbb7c0553
Richard Hessler <richessler@gmail.com>
2016-03-02 16:56:01 -0300

    Added jekyll project to site dir

---
+++ js/main.js
@@ -0,0 +1,37 @@
+---
+layout: null
+---
+
+$(document).ready(function () {
+  $('a.blog-button, a.gallery-button').click(function (e) {
+    var $panelCover = $('.panel-cover');
+    if ($panelCover.hasClass('panel-cover--collapsed')) return;
+    currentWidth = $panelCover.width();
+    if (currentWidth < 960) {
+      $panelCover.addClass('panel-cover--collapsed');
+      $('.content-wrapper').addClass('animated slideInRight')
+    } else {
+      $panelCover.css('max-width', currentWidth);
+      $panelCover.animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+    }
+  });
+
+  if (window.location.hash && (window.location.hash == '#blog' || window.location.hash == '#gallery_index')) {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
+
+  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
+
+  $('.btn-mobile-menu').click(function () {
+    $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  });
+
+  $('.navigation-wrapper .blog-button').click(function () {
+    $('.navigation-wrapper').toggleClass('visible');
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+  })
+
+});
